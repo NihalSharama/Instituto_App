5314759dcd76528369d9e9edf396ab36b0aca990
Nihal Sharma <itsnihal.sharma@gmail.com>
2022-08-02 12:15:42 +0530

    global variables initialized

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,1 +1,25 @@
 // all global variables for colors with be here
+
+import 'package:flutter/material.dart';
+
+class ColorScheme {
+  // COLORS
+  static const mainColor = Color.fromRGBO(35, 127, 213, 1);
+  static const backgroundColor = Colors.white;
+  static const titleColor = Color.fromRGBO(34, 34, 34, 1);
+  static const titleColorLight = Color.fromRGBO(54, 54, 54, 1);
+  static const descriptionColor = Color.fromRGBO(70, 70, 70, 1);
+  static const descriptionColorLight = Color.fromRGBO(126, 126, 126, 1);
+  static const descriptionColorDark = Color.fromRGBO(94, 94, 94, 1);
+  static const curveColor = Color.fromRGBO(35, 127, 213, 1);
+  static const curveColorlight = Color.fromRGBO(35, 127, 213, 0.5);
+  static const backgorundGray = Color.fromRGBO(123, 123, 123, 1);
+  static const backgroundGrayLight = Color.fromRGBO(204, 204, 204, 1);
+  static const backgroundGrayDark = Color.fromRGBO(171, 171, 171, 1);
+}
+
+class StaticImages {
+  // STATIC IMAGES
+  static const appBarCurve = 'assets/image/appbar_curve.svg';
+  static const circularCurve = 'assets/image/appbar_curve.svg';
+}
