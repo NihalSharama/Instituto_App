b9e66030c78b6db155d354a8118d34382bf0989e
Nihal Sharma <itsnihal.sharma@gmail.com>
2022-10-20 19:16:44 +0530

    notification and requests page

--- lib/features/chats/chats_screen.dart
+++ lib/features/chats/chats_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:instituto/features/auth/screens/custom_bottom_navigation.dart';
 
 class RecentChatsScreen extends StatefulWidget {
   static const String routeName = 'chats';
